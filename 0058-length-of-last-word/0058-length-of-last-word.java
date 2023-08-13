79bec8e7b10bd105fb81d39c4c2ab679259af658
Rachna B <125238121+Poke-aurora@users.noreply.github.com>
2023-08-13 13:13:51 +0530

    Update 0058-length-of-last-word.java

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -8,3 +8,6 @@ class Solution {
         return s.length() -lastspaceindex-1;
         }
     }
+note:
+lastIndexOf(char ch, int beg) : This method returns the index of the last occurrence of the character
+in the character sequence represented by this object that is less than or equal to beg, or -1 if the character does not occur before that point.
